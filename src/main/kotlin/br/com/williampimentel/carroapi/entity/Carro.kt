52573a6294df9b34ab4b744c10256f6cec6ba170
logonrm <logonrm@fiap.com.br>
2018-03-10 14:00:44 -0300

    Alteração Carro

--- src/main/kotlin/br/com/williampimentel/carroapi/entity/Carro.kt
+++ src/main/kotlin/br/com/williampimentel/carroapi/entity/Carro.kt
@@ -8,7 +8,8 @@ data class Carro(@Id var id: String?,
                  var marca: String,
                  var modelo: String,
                  var ano: Int,
-                 var placa: String) {
+                 var placa: String,
+                 var urlImagem: String?) {
 
     /*constructor():this("",
             "",
